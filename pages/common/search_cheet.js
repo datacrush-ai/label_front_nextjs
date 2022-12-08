c52ab5863c134184b152205f32fc78f7711610b4
박성욱 <>
2022-12-08 19:14:43 +0900

    progress 추가

--- pages/common/search_cheet.js
+++ pages/common/search_cheet.js
@@ -1,6 +1,8 @@
 import { useRef } from "react";
 import { useCallback, useEffect, useState } from "react";
 import { sendFetch } from "./common_script"
+import NProgress from "nprogress";
+// import "nprogress/nprogress.css";
 
 export default function SearchCheet() {
     const [searchJSON, setSearchJSON] = useState([]);
@@ -22,6 +24,7 @@ export default function SearchCheet() {
         console.log(param);
         const result = await sendFetch('/labeltool/searchJson', param, {method: 'POST'});
         setSearchJSON(result.scenarioDTOList);
+        NProgress.done();
         return result.scenarioDTOList;
     }, [prtAinRef, bgnDtRef, endDtRef]);
     
@@ -51,7 +54,7 @@ export default function SearchCheet() {
     })
 
     console.log(searchJSON)
-    
+
     return(
         <>
             <div style={{'minWidth': '60%'}} className={"flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"}>
@@ -60,7 +63,10 @@ export default function SearchCheet() {
                         <input ref={prtAinRef} placeholder={"아이디(prtAin)"} className={"bg-gray-50 border text-gray-900  border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"}></input>
                         <input ref={bgnDtRef} placeholder={"시작일자"} className={"bg-gray-50 border text-gray-900  border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"}></input>
                         <input ref={endDtRef} placeholder={"종료일자"} className={"bg-gray-50 border text-gray-900  border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"}></input>
-                        <button onClick={() => initFunction()}
+                        <button onClick={() => {
+                            NProgress.start();
+                            initFunction();
+                        }}
                                 className={"w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800"}
                         >찾기
                         </button>
